e0d2340c2f4732b049b122f1431575bcce540acb
Beth Schofield <bethmschofield@gmail.com>
2017-12-20 22:59:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const input = document.querySelector('input')
+// const input = document.querySelector('input')
+
+const input = document.getElementById('mocha')
 
 function init() {
   let index = 0;
